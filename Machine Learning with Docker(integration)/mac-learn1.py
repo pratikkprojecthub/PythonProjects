0df53362f7414193de70b478e98b46a1925cb77b
pratikkprojecthub <83881933+pratikkprojecthub@users.noreply.github.com>
2021-12-28 23:12:00 +0530

    mac-learn1.py

--- Machine Learning with Docker(integration)/mac-learn1.py
+++ Machine Learning with Docker(integration)/mac-learn1.py
@@ -1,12 +1,13 @@
 #################################################################### to save for future use ##############################################
+# Step 1: Installation of the package 
 import joblib
-
+# Step 2:
 model = joblib.load('salary1.pk1')
-
+# Step 3:
 yrsExp = float(input("Enter  Experience : "))
-
+# Step 4:
 prediction = model.predict([[yrsExp]])
-
+# Step 5:
 print("Your Salary as per dataset would be : ", prediction)
 
 
