fe29c90a31d1140250c4cc2f776bed533871c206
pratikkprojecthub <83881933+pratikkprojecthub@users.noreply.github.com>
2022-01-23 21:18:59 +0530

    mac-learn.py

--- Machine Learning with Docker(integration)/mac-learn.py
+++ Machine Learning with Docker(integration)/mac-learn.py
@@ -11,5 +11,6 @@ model.fit(x, y)
 model.predict([[ 8.1 ]])
 
 # for saving the model for future use we use joblib library
+# importing the package to save the model...
 import joblib
 joblib.dump(model, 'salary1.pk1')
